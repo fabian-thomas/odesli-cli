ed22114a7b633b6bbceb2f164d346cd4c8f231d8
Fabian Thomas <fabian@fabianthomas.de>
2021-10-16 17:00:30 +0200

    add song and album link properties

--- odesli_cli/odesli_cli.py
+++ odesli_cli/odesli_cli.py
@@ -5,7 +5,7 @@ from odesli.entity.song.SongResult import SongResult
 from odesli.entity.album.AlbumResult import AlbumResult
 
 INDENTATION_WIDTH = 4
-PROPERTIES = ['all', 'artistName', 'id', 'link', 'thumbnailUrl', 'thumbnailWidth', 'thumbnailHeight', 'title', 'type']
+PROPERTIES = ['all', 'artistName', 'id', 'link', 'thumbnailUrl', 'thumbnailWidth', 'thumbnailHeight', 'title', 'type', 'songLink', 'albumLink']
 
 def indentString(level, s):
     return ' '*INDENTATION_WIDTH*level+s
@@ -24,19 +24,19 @@ def main():
     args = parser.parse_args()
 
     odesli = Odesli()
-    entity = odesli.getByUrl(args.url)
+    result = odesli.getByUrl(args.url)
 
     # use specified api provider for printing the data
-    if isinstance(entity, SongResult):
+    if isinstance(result, SongResult):
         if args.provider == None:
-            entity = entity.song
+            entity = result.song
         else:
-            entity = entity.songsByProvider[args.provider]
-    elif isinstance(entity, AlbumResult):
+            entity = result.songsByProvider[args.provider]
+    elif isinstance(result, AlbumResult):
         if args.provider == None:
-            entity = entity.album
+            entity = result.album
         else:
-            entity = entity.albumsByProvider[args.provider]
+            entity = result.albumsByProvider[args.provider]
     else:
         raise NotImplementedError()
 
@@ -51,7 +51,11 @@ def main():
     elif args.property == 'type':
         print(entity.getType())
     elif args.property == 'all':
-        formatString = "{:<20}"*2
+        formatString = "{:<17}"*2
+        if entity.getType() == 'song':
+            print(formatString.format('songLink:', result.songLink))
+        elif entity.getType() == 'album':
+            print(formatString.format('albumLink:', result.albumLink))
         print(formatString.format('id:', entity.id))
         print(formatString.format('type:', entity.getType()))
         print(formatString.format('title:', entity.title))
